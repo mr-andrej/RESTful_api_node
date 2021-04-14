78c152771c7b7190cb83b41cd36e1353435776f9
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-04-14 08:51:51 +0200

    Working on read/update methods. Pathing still not implemented in server.js

--- controllers/Wilder.js
+++ controllers/Wilder.js
@@ -15,4 +15,26 @@ module.exports = {
         });
     });
   },
+  read: (req, res) => {
+    WilderModel.find()
+    .then(wilder => {
+        if (!wilder) res.json({ success: false, result: "No wilders found"})
+
+        res.json({ sucess: true, result: result})
+    })
+    .catch(err => {
+        res.json({ success: false, result: err})
+    })
+},
+update: (req, res) => {
+  WilderModel.updateOne({_id: req.body._id}, req.body)
+  .then(wilder => {
+      if (!wilder) res.json({ success: false, result: "No such wilder exists"})
+    
+      res.json(wilder)
+  })
+    .catch(err => {
+        res.json({ success: false, result: err})
+    })
+  },
 };
